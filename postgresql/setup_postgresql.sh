25659a0b47695af5d38854e80b92908fc7cf0580
Brian Lusina <lusinabrian@gmail.com>
2017-10-22 15:15:32 +0300

    startup script for PostgreSQL

--- postgresql/setup_postgresql.sh
+++ postgresql/setup_postgresql.sh
@@ -4,22 +4,32 @@
 export POSTGRES_USER=nightking
 export POSTGRES_PASSWORD=atlas
 export POSTGRES_DB=atlasofthrones
-DOCKER_FILE=Dockerfile
+export DOCKER_FILE=Dockerfile
 
-# Before creating files, check that the destination directory exists
-if [ ! -f ${DOCKER_FILE} ]; then
-    echo "Creating PostgreSQL Dockerfile..."
-    touch ${DOCKER_FILE}
-fi
+function createPostgreSQLDockerFile {
+    echo ">>> Downloading database dump"
+    wget https://cdn.patricktriest.com/atlas-of-thrones/atlas_of_thrones.sql
 
-function createPostgreSQLDockerFile{
+    # Before creating files, check that the destination directory exists
+    if [ ! -f ${DOCKER_FILE} ]; then
+        echo "Creating PostgreSQL Dockerfile..."
+        touch ${DOCKER_FILE}
+    fi
 
-    echo "FROM postgres:latest">> ${DOCKER_FILE}
-    echo "\n">> ${DOCKER_FILE}
-    echo "# Set environment variables for postgres">> ${DOCKER_FILE}
+    echo "FROM postgres:latest" >> ${DOCKER_FILE}
+    echo "" >> ${DOCKER_FILE}
+    echo "# Set environment variables for postgres" >> ${DOCKER_FILE}
     echo "ENV POSTGRES_USER ${POSTGRES_USER}" >> ${DOCKER_FILE}
     echo "ENV POSTGRES_PASSWORD ${POSTGRES_PASSWORD}" >> ${DOCKER_FILE}
     echo "ENV POSTGRES_DB ${POSTGRES_DB}" >> ${DOCKER_FILE}
+    echo "RUN CREATE USER ${POSTGRES_USER} WITH PASSWORD '${POSTGRES_PASSWORD}';" >> ${DOCKER_FILE}
+    echo "RUN CREATE DATABASE ${POSTGRES_DB};" >> ${DOCKER_FILE}
+    echo "RUN GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} to ${POSTGRES_USER};" >> ${DOCKER_FILE}
+    echo "RUN GRANT SELECT ON ALL TABLES IN SCHEMA public TO ${POSTGRES_USER};" >> ${DOCKER_FILE}
+    echo "RUN \c ${POSTGRES_DB}" >> ${DOCKER_FILE}
+    echo "RUN CREATE EXTENSION postgis;" >> ${DOCKER_FILE}
+    echo "RUN psql -d ${POSTGRES_DB} < atlas_of_thrones.sql" >> ${DOCKER_FILE}
+
 }
 
 createPostgreSQLDockerFile
