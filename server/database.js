f1e9ed35bb45a11fa0895dbe9e91b0172a0416a3
Brian Lusina <lusinabrian@gmail.com>
2017-11-08 15:25:58 +0300

    create postgress databae client

--- server/database.js
+++ server/database.js
@@ -1,3 +1,15 @@
 /**
  * Postgres DB Module
  */
+const postgres = require("pg");
+const log = require("./logger");
+const connectionString = process.env.DATABASE_URL;
+
+// initialize a new postgress client
+const client = postgres.Client({ connectionString });
+
+client.connect().then(() => {
+   log.info(`Connected to ${client.database} at ${client.host}:${client.port}`)
+}).catch(err => {
+    log.error(`Failed to connect to database with error ${err}`)
+});
