f60b0b40a9b91e0d47a462b1b3a2a7cc2158f07e
Brian Lusina <lusinabrian@gmail.com>
2017-11-08 14:57:59 +0300

    add koa middleware

--- server/server.js
+++ server/server.js
@@ -1,2 +1,33 @@
-require('dotenv').config()
-require('./server')
+require('dotenv').config();
+const Koa = require('koa');
+const cors = require('kcors');
+const log = require('./logger');
+const api = require('./api');
+
+
+const app = Koa();
+const port = process.env.PORT || 8000;
+const origin = process.env.CORS_ORIGIN | "*";
+app.use(cors({ origin }));
+
+// log requests
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    // This will pause this function until the endpoint handler has resolved
+    await next();
+    const responseTime = Date.now() - start;
+    log.info(`${ctx.method} ${ctx.status} ${ctx.url} - ${responseTime} ms`);
+});
+
+/**
+ * Error handler, all errors will percolate up to here
+ * */
+app.use(async (ctx, next) => {
+   try{
+       await next();
+   }catch (err){
+       ctx.status = err.status || 500;
+       ctx.body = err.message;
+       log.error(`Request ${ctx.url} - ${err.message}`)
+   }
+});
