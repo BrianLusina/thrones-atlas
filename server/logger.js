0f00d20e28302aa152b5c41435273ecaf32e1ebf
Brian Lusina <lusinabrian@gmail.com>
2017-11-08 15:05:36 +0300

    Logger middlware
    Logging will be application wide and will be handled by winston to pipe logs to either console or files for both info and erros.
    Allows to configure the application for logging the information to either a dev server or production server based on use case

--- server/logger.js
+++ server/logger.js
@@ -1,3 +1,22 @@
 /**
  * Winston Logger Module
+ * Will configure app-wide logging of the application
  */
+const winston = require("winston");
+const path = require("path");
+
+module.exports = new winston.Logger({
+   transports : [
+       new (winston.transports.Console),
+       new (winston.transports.File)({
+           name : "info-file",
+           filename: path.resolve(__dirname, "info.log"),
+           level: "info"
+       }),
+       new (winston.transports.File)({
+           name:"error-file",
+           filename:path.resolve(__dirname, "error.log"),
+           level: "error"
+       })
+   ]
+});
