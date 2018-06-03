78142ecab9ef874cab0cef9555b0dc40f45a9aa6
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-06-03 23:55:24 +0300

    Minor updates

--- server/logger.js
+++ server/logger.js
@@ -6,19 +6,19 @@ const winston = require('winston')
 const path = require('path')
 
 module.exports = new winston.createLogger({
-    format: winston.format.json(),
-    transports: [
-        new(winston.transports.Console)(),
-        new(winston.transports.File)({
-            name: 'info-file',
-            filename: path.resolve(__dirname, '../info.log'),
-            level: 'info'
-        }),
-        new(winston.transports.File)({
-            name: 'error-file',
-            filename: path.resolve(__dirname, '../error.log'),
-            level: 'error'
-        })
+  format: winston.format.json(),
+  transports: [
+    new (winston.transports.Console)(),
+    new (winston.transports.File)({
+      name: 'info-file',
+      filename: path.resolve(__dirname, '../info.log'),
+      level: 'info'
+    }),
+    new (winston.transports.File)({
+      name: 'error-file',
+      filename: path.resolve(__dirname, '../error.log'),
+      level: 'error'
+    })
 
-    ]
+  ]
 })
