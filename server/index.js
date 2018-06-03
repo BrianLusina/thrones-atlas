b20e8fb651962594228bf3a8af0ba04041d0c94c
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-06-03 23:41:29 +0300

    Server configuration and setup

--- server/index.js
+++
@@ -1,4 +0,0 @@
-const koa = require('koa')
-const cors = require('kcors')
-const log = require('./logger')
-const api = require('./api')
--- server/server.js
+++ server/server.js
@@ -1,2 +1,50 @@
 require('dotenv').config()
-require('./index')
+const Koa = require('koa')
+const cors = require('kcors')
+const log = require('./logger')
+const api = require('./api')
+
+// SETUP KOA
+const app = new Koa()
+const port = process.env.PORT || 5000
+
+// apply cors
+const origin = process.env.CORS_ORIGIN || '*'
+app.use(cors({
+  origin
+}))
+
+// log all requests
+app.use(async (ctx, next) => {
+  const start = Date.now()
+  // this will pause the control flow until the endpoint handler is resolved
+  await next()
+  const responseTime = Date.now() - start
+  log.info(`${ctx.method} ${ctx.status} ${ctx.url} - ${responseTime} ms`)
+})
+
+// error handler
+// all uncaught exceptions will percolate upto here
+app.use(async (ctx, next) => {
+  try {
+    await next()
+  } catch (error) {
+    ctx.status = error.status || 500
+    ctx.body = error.message
+    log.error(`Request Error ${ctx.url} - ${error.message}`)
+  }
+})
+
+// apply default header responses
+app.use(async (ctx, next) => {
+  await next()
+  ctx.set('Cache-Control', 'public, max-age=3600')
+})
+
+// mount routes
+app.use(api.routes(), api.allowedMethods())
+
+// start the application
+app.listen(port, () => {
+  log.info(`Server listening at port ${port}`)
+})
