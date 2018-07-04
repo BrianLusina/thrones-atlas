44a62dbdcbaec84fa9f770d5257ab333f41087ad
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-07-04 15:22:31 +0300

    feat: Component base class
    Component base class all components will inherit from. This will add
    createEvents and triggerEvent functions that can be used forevent
    handling.
    
    The constructor is used to add properties, events and placeholder attrs
    to the component. On creation of a component, the createEvents instance
    function will be triggered adding events to the component, ie. if events
    exist.
    
    The template will be added to the componentElement

---
+++ client/src/components/Component.js
@@ -0,0 +1,57 @@
+/**
+ * Base component class that will be inherited and extended by all components in application.
+ * This will have the placeholderId, props and template which will be used to construct the template and 
+ * render to the DOM.
+ */
+export class Component {
+    /**
+     * Component constructor
+     * @param {String} placeHolderId Element Id to inflate component into
+     * @param {Object} props properties of the given component, which will contain data and events
+     * data is an object with data related to the given component,
+     * events are the event listeners that the component will use
+     * @param {String} template HTML template to inflate into placeholder
+     */
+    constructor(placeHolderId, props, template) {
+        this.componentElement = document.getElementById(placeHolderId);
+
+        if (template) {
+            // load template into innerHTML
+            this.componentElement.innerHTML = template;
+
+            // find all refs in component
+            this.refs = {}
+            const refElems = this.componentElement.querySelectorAll("[ref]");
+
+            // assign each ref to this.refs object
+            refElems.forEach(elem => {
+                this.refs[elem.getAttribute("ref")] = elem
+            })
+        }
+        if (props.events) {
+            this.createEvents(props.events)
+        }
+    }
+
+    /**
+     * Read events component params and attach event listeners to each
+     * @param {Object} events Events for the given component
+     */
+    createEvents(events) {
+        Object.keys(events).forEach(eventName => {
+            this.componentElement.addEventListener(eventName, events[eventName], false)
+        });
+    }
+
+    /**
+     * Trigger events on the component
+     * @param {String} eventName event name
+     * @param {Object} detail payload passed to event
+     */
+    triggerEvents(eventName, detail) {
+        const event = new window.CustomEvent(eventName, {
+            detail
+        })
+        this.componentElement.dispatchEvent(event)
+    }
+}
